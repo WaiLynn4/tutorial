851704ea50880646a4c25c12857a11f06ba17a86
WaiLynn4 <wlay21416@gmail.com>
2025-06-02 15:16:29 +0630

    react day 2

---
+++ react-lessons/src/app/page.js
@@ -0,0 +1,8 @@
+import Example10 from "../components/Example10";
+export default function Home() {
+  return (
+    <div>
+      <Example10/>
+    </div>
+  );
+}
---
+++ react-lessons/src/components/Example10.js
@@ -0,0 +1,35 @@
+"use client";
+
+import { useState } from "react";
+
+function LoginButton({ onClickButton }) {
+  return (
+    <div>
+      <button onClick={onClickButton}>Login Button</button>
+    </div>
+  );
+}
+
+function UsernameInput({ value, onChangeName }) {
+  return (
+    <div>
+      <input value={value} onChange={(e) => onChangeName(e.target.value)} />
+    </div>
+  );
+}
+
+//Example10: Passing a Function as a Prop
+function Parent() {
+  const [username, setUsername] = useState();
+  return (
+    <div>
+      <UsernameInput
+        value={username}
+        onChangeName={(changedName) => setUsername(changedName)}
+      />
+      <LoginButton onClickButton={() => alert("Clicked Login Button.")} />
+    </div>
+  );
+}
+
+export default Parent;
